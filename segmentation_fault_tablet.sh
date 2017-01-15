ee9d30d39b2069c2bbddaea94a4468de91946bb8
imorrt <imorrt@gmail.com>
2017-01-15 20:14:18 +0600

    Update segmentation_fault_tablet.sh

--- segmentation_fault_tablet.sh
+++ segmentation_fault_tablet.sh
@@ -5,3 +5,5 @@ wget https://raw.githubusercontent.com/imorrt/zabbix_installer/master/segmentati
 chmod +x segmentation_fault_check.sh
 chown zabbix:zabbix segmentation_fault_check.sh
 echo "UserParameter=segfault,/etc/zabbix/scripts/segmentation_fault_check.sh" >> /etc/zabbix/zabbix_agentd.d/userparams.conf
+/etc/init.d/zabbix-agent stop
+/etc/init.d/zabbix-agent start
