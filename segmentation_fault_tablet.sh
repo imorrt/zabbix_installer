29bb0d5cfb0720df62cf896738f7b77f7845d541
imorrt <imorrt@gmail.com>
2017-01-15 20:02:45 +0600

    Add files via upload

---
+++ segmentation_fault_tablet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /etc/zabbix/scripts/
+wget https://raw.githubusercontent.com/imorrt/zabbix_installer/master/segmentation_fault_check.sh
+chmod +x segmentation_fault_check.sh
+chown zabbix:zabbix segmentation_fault_check.sh
+echo "UserParameter=segfault,/etc/zabbix/scripts/segmentation_fault_check.sh" >> /etc/zabbix/zabbix_agentd.d/userparams.conf
