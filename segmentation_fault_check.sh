719c8f869b1a51119e200897c91373a6631ae94a
imorrt <imorrt@gmail.com>
2017-01-15 20:30:38 +0600

    Update segmentation_fault_check.sh

--- segmentation_fault_check.sh
+++ segmentation_fault_check.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-DATE_APACHE=`grep "Segmentation fault" /var/log/apache2/error.log | cut -d"]" -f1 |  cut -d"[" -f2 | tail -n1`
-DATE_NOW=`date +%s`
+PRE_DATE_APACHE=`grep "Segmentation fault" /var/log/apache2/error.log | cut -d"]" -f1 |  cut -d"[" -f2 | tail -n1`
+DATE_APACHE=`date -d "$PRE_DATE_APACHE"  +"%s"`
+DATE_NOW=`date +"%s"`
 
-DATE_DIFF_MINUTES = $[ ($DATE_NOW - $DATE_APACHE) / 60 ]
+DATE_DIFF_MINUTES=$[ ($DATE_NOW - $DATE_APACHE) / 60 ]
 
-if [ $DATE_DIFF_MINUTES -gt 15 ]; then
+if [ $DATE_DIFF_MINUTES -gt 5 ]; then
     DO_ALERT=0
 else
         DO_ALERT=1
