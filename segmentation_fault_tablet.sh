4c8407cd1913158bcf34115593ac6355307b8fc0
imorrt <imorrt@gmail.com>
2017-01-15 20:26:17 +0600

    Delete segmentation_fault_tablet.sh

--- segmentation_fault_tablet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /etc/zabbix/scripts/
-wget https://raw.githubusercontent.com/imorrt/zabbix_installer/master/segmentation_fault_check.sh
-chmod +x segmentation_fault_check.sh
-chown zabbix:zabbix segmentation_fault_check.sh
-echo "UserParameter=segfault,/etc/zabbix/scripts/segmentation_fault_check.sh" >> /etc/zabbix/zabbix_agentd.d/userparams.conf
-/etc/init.d/zabbix-agent stop
-/etc/init.d/zabbix-agent start
