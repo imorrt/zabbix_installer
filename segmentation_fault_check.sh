139a69391903253a533385b2f2ef9d6491b27f1a
imorrt <imorrt@gmail.com>
2017-01-15 19:49:50 +0600

    Add files via upload

---
+++ segmentation_fault_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATE_APACHE=`grep "Segmentation fault" /var/log/apache2/error.log | cut -d"]" -f1 |  cut -d"[" -f2 | tail -n1`
+DATE_NOW=`date +%s`
+
+DATE_DIFF_MINUTES = $[ ($DATE_NOW - $DATE_APACHE) / 60 ]
+
+if [ $DATE_DIFF_MINUTES -gt 5 ]; then
+    DO_ALERT=0
+else
+	DO_ALERT=1
+fi
+
+echo $DO_ALERT
