17e2df7dd2d57b495f2008b4398553b1e75b5b2f
imorrt <imorrt@gmail.com>
2017-02-16 19:26:29 +0600

    Update mysqli_real_connect.sh

--- mysqli_real_connect.sh
+++ mysqli_real_connect.sh
@@ -4,7 +4,7 @@ PREDATE=`grep "mysqli_real_connect" /var/log/apache2/error.log | cut -d"]" -f1 |
 DATE_APACHE=`date -d "$PREDATE"  +"%s"`
 DATE_NOW=`date +%s`
 
-DATE_DIFF_MINUTES = $[ ($DATE_NOW - $DATE_APACHE) / 60 ]
+DATE_DIFF_MINUTES=$[ ($DATE_NOW - $DATE_APACHE) / 60 ]
 
 if [ "$DATE_DIFF_MINUTES" -gt 5 ]; then
     DO_ALERT=0
