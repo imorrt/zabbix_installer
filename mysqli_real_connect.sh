34cbebb8478358e2928ff3d50e987a25aba0542a
imorrt <imorrt@gmail.com>
2017-02-16 18:59:57 +0600

    Add files via upload

---
+++ mysqli_real_connect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATE_APACHE=`grep "mysqli_real_connect" /var/log/apache2/error.log | cut -d"]" -f1 |  cut -d"[" -f2 | tail -n1`
+DATE_NOW=`date +%s`
+
+DATE_DIFF_MINUTES = $[ ($DATE_NOW - $DATE_APACHE) / 60 ]
+
+if [ $DATE_DIFF_MINUTES -gt 5 ]; then
+    DO_ALERT=0
+else
+	DO_ALERT=1
+fi
+
+echo $DO_ALERT
---
+++ mysqli_real_connect_tablet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /etc/zabbix/scripts/
+wget https://raw.githubusercontent.com/imorrt/zabbix_installer/master/mysqli_real_connect.sh
+chmod +x mysqli_real_connect.sh
+chown zabbix:zabbix mysqli_real_connect.sh
+echo "UserParameter=mrcon,/etc/zabbix/scripts/mysqli_real_connect.sh" >> /etc/zabbix/zabbix_agentd.d/userparams.conf
