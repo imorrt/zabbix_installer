8cb7a9ca121523f006118e087b833f11393d9ebd
imorrt <imorrt@gmail.com>
2017-02-16 19:04:31 +0600

    Update mysqli_real_connect_tablet.sh

--- mysqli_real_connect_tablet.sh
+++ mysqli_real_connect_tablet.sh
@@ -4,3 +4,7 @@ wget https://raw.githubusercontent.com/imorrt/zabbix_installer/master/mysqli_rea
 chmod +x mysqli_real_connect.sh
 chown zabbix:zabbix mysqli_real_connect.sh
 echo "UserParameter=mrcon,/etc/zabbix/scripts/mysqli_real_connect.sh" >> /etc/zabbix/zabbix_agentd.d/userparams.conf
+
+
+/etc/init.d/zabbix-agent stop
+/etc/init.d/zabbix-agent start
