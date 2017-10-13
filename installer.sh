1f81b7997685addb978afaa41c158fd63c014c1a
imorrt <imorrt@gmail.com>
2017-10-13 10:05:44 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -37,11 +37,11 @@ Install()
             wget http://repo.zabbix.com/zabbix/3.2/debian/pool/main/z/zabbix-release/zabbix-release_3.2-1+jessie_all.deb
             dpkg -i zabbix-release_3.2-1+jessie_all.deb
             apt-get update
-            apt-get install zabbix-agent
+            apt-get install zabbix-agent -y
 
         elif [[ $checkOS = "9" ]]; then
             apt-get update
-            apt-get install zabbix-agent
+            apt-get install zabbix-agent -y
         else
             exit
         fi       
